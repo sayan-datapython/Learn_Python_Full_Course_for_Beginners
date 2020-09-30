1cca3df62cfd7ae5a0c85c019b0f5fe4f6f4740c
sayan-datapython <72145939+sayan-datapython@users.noreply.github.com>
2020-09-30 20:09:37 +0530

    Update Exercise 004.py
    Using comments make your work easy to understand

--- Exercise 004.py
+++ Exercise 004.py
@@ -1,8 +1,14 @@
 n = input("Enter anything: ")
 print("O tipo primitivo desde valor: ", type(n))
+#print if n contain space
 print("Somente espaco:", n.isspace())
+#print if n contain numbers
 print("E numerico:", n.isnumeric())
+#print if n contain alphabets
 print("E alfabetico: ", n.isalpha())
+#print if n contains both numerical as well as aplhabets
 print("E alfanumerico: ", n.isalnum())
+#print if n is uppercase
 print("E Upper: ", n.isupper())
+#print if n is lowercase
 print("E low: ", n.islower())
